e0cb9378a962ffda05052c5e19a0b6982c094873
Afsana17 <149375975+Afsana17@users.noreply.github.com>
2023-12-07 21:52:02 +0530

    Add files via upload

---
+++ 4_addTwoNum.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  int num1;
+  int num2;
+  int sum=0;
+  printf("Enter number 1:");
+  scanf("%d",&num1);
+  printf("Enter number 2:");
+  scanf("%d",&num2);
+  sum= num1+num2;
+  printf("Addition of two numbers is: %d",sum);
+  return 0;
+}
