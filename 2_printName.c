62e74142da7666313aa95b501c2db7952d20c8b2
Afsana17 <149375975+Afsana17@users.noreply.github.com>
2023-12-07 21:36:20 +0530

    Add files via upload

---
+++ 2_printName.c
@@ -0,0 +1,23 @@
+     WAY 1:
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+    printf("Name:Afsana \n");
+    return 0;
+}
+           WAY 2:
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+  char Name[20];
+  printf("Enter Your Name :");
+  scanf("%s",Name);
+  printf("Your Name is %s",Name);
+  return 0;
+
+
+}
