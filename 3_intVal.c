918011c9af0833b9a6c00c702ddcfd0df7d5bd51
Afsana17 <149375975+Afsana17@users.noreply.github.com>
2023-12-07 21:46:05 +0530

    Add files via upload

---
+++ 3_intVal.c
@@ -0,0 +1,10 @@
+TYPE 1:
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+ int a=25;
+ printf(" The value is %d",a);
+
+}
---
+++ 3_intVal2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+ int val;
+ printf("Enter a value :");
+ scanf("%d",&val);
+ printf("The value entered is :%d",val);
+
+}
